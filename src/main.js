5280947da0a622d1c1e0cf67c11eca2092136584
lujiben <937271985@qq.com>
2020-05-15 01:46:19 +0800

    新增axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// 替换vue本来的http模块
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
